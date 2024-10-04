1732811e10740a93e31c64d612371ddcf5981c2f
unknown <arbazkdr7866@gmail.com>
2024-10-04 14:39:39 +0530

    Addesd robo.txt

---
+++ public/next-sitemap.conf.tsx
@@ -0,0 +1,5 @@
+module.exports = {
+    siteUrl: 'https://www.kdrtech.in', // Replace with your actual site URL
+    generateRobotsTxt: true, // (optional) Generate a robots.txt file
+    // Additional options can be added here
+  };
